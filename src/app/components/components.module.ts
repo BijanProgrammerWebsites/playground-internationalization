c46383d4dea800502cf638b555e45fcc30c55d56
BijanProgrammer <bijaneisapour@gmail.com>
2021-12-10 21:24:54 +0330

    [ADD] dark theme

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {RouterModule} from '@angular/router';
 import {NavigationComponent} from './navigation/navigation.component';
+import {IconsModule} from '../icons/icons.module';
 
 @NgModule({
     declarations: [HeaderComponent, NavigationComponent],
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, IconsModule],
     exports: [HeaderComponent, NavigationComponent],
 })
 export class ComponentsModule {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,14 @@
 import {Component} from '@angular/core';
+import {Theme} from 'src/app/models/theme';
+import {ThemeService} from '../../services/theme.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    public Theme = Theme;
+
+    public constructor(public themeService: ThemeService) {}
+}
